6f407f6fda404e1ec539d94254c54f86af88b235
Smart Collaborative <daniel.spatacean@smart-collaborative.ro>
2024-09-14 12:47:17 +0300

    Buton conectare plc

--- Trasabilitate/MainWindow.xaml.cs
+++ Trasabilitate/MainWindow.xaml.cs
@@ -1,24 +1,42 @@
-﻿using System.Text;
+﻿using S7.Net;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Trasabilitate
+namespace WpfApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private PlcConnection plcConnection;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+
+        private void ConnectButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            // Inițializează conexiunea la PLC (folosește IP-ul PLC-ului tău)
+            plcConnection = new PlcConnection("192.168.0.1");
+
+            // Încearcă să te conectezi la PLC
+            plcConnection.Connect();
+
+            // Verifică dacă conexiunea a fost realizată cu succes
+            if (plcConnection.GetPlcInstance().IsConnected)
+            {
+                // Dacă conexiunea este realizată cu succes, schimbă culoarea butonului în verde
+                ConnectButton.Background = new SolidColorBrush(Colors.Green);
+                ConnectButton.Content = "Connected";
+            }
+            else
+            {
+                // Dacă conexiunea a eșuat, schimbă culoarea în roșu
+                ConnectButton.Background = new SolidColorBrush(Colors.Red);
+                ConnectButton.Content = "Failed to Connect";
+            }
         }
     }
 }
